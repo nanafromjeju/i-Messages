3d6a9cc3be396b95e5782076ebbdbd90b4137efc
nanafromjeju <nanafromjeju@gmail.com>
2025-05-05 02:54:07 +0900

    feat: 채팅방 목록 조회 API 연동 (#7)

--- packages/web/src/pages/List.tsx
+++ packages/web/src/pages/List.tsx
@@ -1,12 +1,27 @@
 import { Button } from "@/components/common/ui/button";
 import Header from "@/components/layout/Header";
-import Rooms from "@/components/layout/Rooms";
+import { Room } from "../../../shared/src/api/types";
+import { useChatRooms } from "../../../shared/src/hooks/useChatRooms";
 
 const List = () => {
+  const { data, isLoading, isError, error } = useChatRooms();
+  console.log({ data });
+  const rooms = data?.data;
+
   return (
     <div>
       <Header />
-      <Rooms />
+      <div>
+        {rooms && rooms.length > 0 ? (
+          <ul>
+            {rooms.map((room: Room) => (
+              <li key={room.id}>{room.name}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>채팅방이 없습니다.</p>
+        )}
+      </div>
       <Button>Button</Button>
     </div>
   );
